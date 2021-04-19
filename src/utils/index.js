eb4a10af5aee122ca8351ce388095ebd9521a1db
Brandon Donato-Long <wartornhero@gmail.com>
2021-04-19 23:10:38 +0200

    added Prettier and fixed a bug in decimal 2 roman that found a performance hit in romantodecimal as well

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,32 +1,42 @@
-const romanToDecimalMap = {M:1000, CM: 900, D:500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX:9, V:5, IV:4, I:1}
+const romanToDecimalMap = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
 
 export const decimal2RomanConverter = (digit) => {
-    let romanNumeral = '';
-    let counter = digit
-    while(counter > 0) {
-        for (const key in romanToDecimalMap) {
-            if (counter >= romanToDecimalMap[key]) {
-                counter -= romanToDecimalMap[key];
-                romanNumeral += key;
-            }
-        }
+  let romanNumeral = "";
+  let counter = digit;
+  for (const key in romanToDecimalMap) {
+    while (counter >= romanToDecimalMap[key]) {
+      counter -= romanToDecimalMap[key];
+      romanNumeral += key;
     }
+  }
 
-    return romanNumeral
-}
+  return romanNumeral;
+};
 
 export const romanToDecimalConverter = (romanNumeral) => {
-    let decimal = 0;
-    let counter = romanNumeral
+  let decimal = 0;
+  let counter = romanNumeral;
 
-    while(counter.length> 0) {
-        for (const key in romanToDecimalMap) {
-            if (counter.indexOf(key) === 0) {
-                counter = counter.replace(key, "");
-                decimal += romanToDecimalMap[key];
-            }
-        }
+  for (const key in romanToDecimalMap) {
+    while (counter.indexOf(key) === 0) {
+      counter = counter.replace(key, "");
+      decimal += romanToDecimalMap[key];
     }
+  }
 
-    return decimal
-}
+  return decimal;
+};
--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,17 +1,68 @@
-import {decimal2RomanConverter, romanToDecimalConverter} from "./index";
+import { decimal2RomanConverter, romanToDecimalConverter } from "./index";
 const decimalToRomanCases = [
-    [-1, ''],[0, ''],[1,'I'],[2,'II'],[4, 'IV'],[5, 'V'],[7, 'VII'],[8, 'VIII'],[9, 'IX']
-]
+  [-1, ""],
+  [0, ""],
+  [2, "II"],
+  [4, "IV"],
+  [5, "V"],
+  [7, "VII"],
+  [9, "IX"],
+  [10, "X"],
+  [14, "XIV"],
+  [21, "XXI"],
+  [40, "XL"],
+  [50, "L"],
+  [88, "LXXXVIII"],
+  [90, "XC"],
+  [100, "C"],
+  [300, "CCC"],
+  [400, "CD"],
+  [500, "D"],
+  [700, "DCC"],
+  [1888, "MDCCCLXXXVIII"],
+];
 
-const romanToDecimalCases = [['MDCCCLXXXVIII',1888],['DCC', 700],['D', 500],['CD', 400],['CCC', 300],['C', 100],['XCIX', 99],['XC',90],['LXXXVIII', 88], ['L', 50], ['L', 50], ['XLIX', 49],['XL', 40],['XXI', 21],['XIX', 19],['XI', 11],['X', 10], ['IX', 9], ['VIII', 8], ['V', 5], ['IV', 4], ['III', 3]]
-describe('singleDigitConverter', () =>{
-    it.each(decimalToRomanCases)('test RomanNumeralization for singles digit: %i To Be %s', (input, expected) => {
-        expect(decimal2RomanConverter(input)).toBe(expected);
-    })
-})
+const romanToDecimalCases = [
+  ["MDCCCLXXXVIII", 1888],
+  ["DCC", 700],
+  ["D", 500],
+  ["CD", 400],
+  ["CCC", 300],
+  ["C", 100],
+  ["XCIX", 99],
+  ["XC", 90],
+  ["LXXXVIII", 88],
+  ["L", 50],
+  ["XLIX", 49],
+  ["XL", 40],
+  ["XXI", 21],
+  ["XIX", 19],
+  ["XI", 11],
+  ["X", 10],
+  ["IX", 9],
+  ["VIII", 8],
+  ["V", 5],
+  ["IV", 4],
+  ["III", 3],
+  ["", 0],
+];
+describe("singleDigitConverter", () => {
+  // it("breakout test for failing cases", () => {
+  //   expect(decimal2RomanConverter(24)).toBe("XXIV");
+  // });
+  it.each(decimalToRomanCases)(
+    "test RomanNumeralization for singles digit: %i To Be %s",
+    (input, expected) => {
+      expect(decimal2RomanConverter(input)).toBe(expected);
+    }
+  );
+});
 
-describe('RomanNumeral to Decimal Single Digit Converter', () => {
-    it.each(romanToDecimalCases)('test decimalization of roman numerals: %s To Be %d', (input, expected) => {
-        expect(romanToDecimalConverter((input))).toBe(expected);
-    })
-})
+describe("RomanNumeral to Decimal Single Digit Converter", () => {
+  it.each(romanToDecimalCases)(
+    "test decimalization of roman numerals: %s To Be %d",
+    (input, expected) => {
+      expect(romanToDecimalConverter(input)).toBe(expected);
+    }
+  );
+});
