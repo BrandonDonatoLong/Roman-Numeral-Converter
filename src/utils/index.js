f32279c34915523e242723b89a45244b43881482
Brandon Donato-Long <wartornhero@gmail.com>
2021-04-19 21:26:47 +0200

    Brute Force Method did it

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,36 +1,22 @@
-
-
 export const singleDigitConverter = (digit) => {
     let romanNumeral = '';
     let counter = digit
-    // if (counter === 9) {
-    //     return 'IX'
-    // }
-    while(counter > 0 ){
-        console.log(counter)
-        switch (counter){
-            case counter === 9:
-                console.log('nine break');
-                romanNumeral += 'IX';
-                counter -= 9;
-                break;
-            case counter >= 5:
-                console.log('5 break');
-                romanNumeral += 'V';
-                counter -= 5;
-                break;
-            case counter === 4:
-                console.log('4 break');
-                romanNumeral += 'IV';
-                counter -= 4;
-                break;
-            case counter >= 1:
-                console.log('1 break');
-                romanNumeral += 'I';
-                counter--;
-                break;
+    while(counter !== 0) {
+        if (counter === 9) {
+            romanNumeral += 'IX'
+            counter -= 9
+        } else if (counter >= 5) {
+            romanNumeral += 'V'
+            counter -= 5;
+        }else if ( counter === 4) {
+            romanNumeral += 'IV';
+            counter -= 4
+        } else if (counter >= 1 ){
+            romanNumeral += 'I'
+            counter--;
         }
     }
+
     console.log(romanNumeral);
     return romanNumeral
 }
--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,13 +1,13 @@
 import {singleDigitConverter} from "./index";
 
 describe('singleDigitConverter', () =>{
-    it('testSingleDigitConverter', () => {
-        const value = false;
-        console.log(singleDigitConverter(1));
-        expect(value).toBe(true)
-        }
-    )
-    // test.each([[0, ''],[1,'I'],[2,['II']],[4, 'IV'],[5, 'V'],[7, 'VII'],[8, 'VII'],[9, 'IX']])('test RomanNumeralization for singles digit. %i', (input, expected) => {
-    //     expect(singleDigitConverter(input)).toBe(expected);
-    // })
+    // it('testSingleDigitConverter', () => {
+    //     const value = false;
+    //     console.log(singleDigitConverter(1));
+    //     expect(value).toBe(true)
+    //     }
+    // )
+    it.each([[0, ''],[1,'I'],[2,'II'],[4, 'IV'],[5, 'V'],[7, 'VII'],[8, 'VIII'],[9, 'IX']])('test RomanNumeralization for singles digit. %i', (input, expected) => {
+        expect(singleDigitConverter(input)).toBe(expected);
+    })
 })
