45f37643fb9e170e78785170c559d09003c0e8c1
Brandon Donato-Long <wartornhero@gmail.com>
2021-04-19 22:43:19 +0200

    use a more generic technique for decimal to roman as well

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,48 +1,30 @@
-export const decimal2RomanSingleDigitConverter = (digit) => {
+const romanToDecimalMap = {M:1000, CM: 900, D:500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX:9, V:5, IV:4, I:1}
+
+export const decimal2RomanConverter = (digit) => {
     let romanNumeral = '';
     let counter = digit
     while(counter > 0) {
-        if (counter === 9) {
-            romanNumeral += 'IX'
-            counter -= 9
-        } else if (counter >= 5) {
-            romanNumeral += 'V'
-            counter -= 5;
-        }else if ( counter === 4) {
-            romanNumeral += 'IV';
-            counter -= 4
-        } else if (counter >= 1 ){
-            romanNumeral += 'I'
-            counter--;
+        for (const key in romanToDecimalMap) {
+            if (counter >= romanToDecimalMap[key]) {
+                counter -= romanToDecimalMap[key];
+                romanNumeral += key;
+            }
         }
     }
 
     return romanNumeral
 }
 
-export const romanToDecimalSingleDigitConverter = (romanNumeral) => {
+export const romanToDecimalConverter = (romanNumeral) => {
     let decimal = 0;
     let counter = romanNumeral
 
     while(counter.length> 0) {
-        if( counter.indexOf('XL') === 0) {
-            counter = counter.replace('XL', "");
-            decimal += 40;
-        } else if( counter.indexOf('X') === 0) {
-            counter = counter.replace('X', "");
-            decimal += 10;
-        } else if (counter.indexOf('IX') === 0) {
-            counter = counter.replace('IX', "")
-            decimal += 9
-        } else if (counter.indexOf('V') === 0 ) {
-            counter = counter.replace('V', "")
-            decimal += 5;
-        }else if ( counter.indexOf('IV') === 0 ) {
-            counter = counter.replace('IV', "")
-            decimal += 4;
-        } else if (counter.indexOf('I') === 0 ){
-            counter = counter.replace('I', "")
-            decimal++;
+        for (const key in romanToDecimalMap) {
+            if (counter.indexOf(key) === 0) {
+                counter = counter.replace(key, "");
+                decimal += romanToDecimalMap[key];
+            }
         }
     }
 
--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,13 +1,17 @@
-import {decimal2RomanSingleDigitConverter, romanToDecimalSingleDigitConverter} from "./index";
+import {decimal2RomanConverter, romanToDecimalConverter} from "./index";
+const decimalToRomanCases = [
+    [-1, ''],[0, ''],[1,'I'],[2,'II'],[4, 'IV'],[5, 'V'],[7, 'VII'],[8, 'VIII'],[9, 'IX']
+]
 
-describe.skip('singleDigitConverter', () =>{
-    it.each([[-1, ''],[0, ''],[1,'I'],[2,'II'],[4, 'IV'],[5, 'V'],[7, 'VII'],[8, 'VIII'],[9, 'IX']])('test RomanNumeralization for singles digit. %i', (input, expected) => {
-        expect(decimal2RomanSingleDigitConverter(input)).toBe(expected);
+const romanToDecimalCases = [['MDCCCLXXXVIII',1888],['DCC', 700],['D', 500],['CD', 400],['CCC', 300],['C', 100],['XCIX', 99],['XC',90],['LXXXVIII', 88], ['L', 50], ['L', 50], ['XLIX', 49],['XL', 40],['XXI', 21],['XIX', 19],['XI', 11],['X', 10], ['IX', 9], ['VIII', 8], ['V', 5], ['IV', 4], ['III', 3]]
+describe('singleDigitConverter', () =>{
+    it.each(decimalToRomanCases)('test RomanNumeralization for singles digit: %i To Be %s', (input, expected) => {
+        expect(decimal2RomanConverter(input)).toBe(expected);
     })
 })
 
 describe('RomanNumeral to Decimal Single Digit Converter', () => {
-    it.each([['XL', 40],['XXI', 21],['XIX', 19],['XI', 11],['X', 10], ['IX', 9], ['VIII', 8], ['V', 5], ['IV', 4], ['III', 3]])('test decimalization of roman numerals: %s', (input, expected) => {
-        expect(romanToDecimalSingleDigitConverter((input))).toBe(expected);
+    it.each(romanToDecimalCases)('test decimalization of roman numerals: %s To Be %d', (input, expected) => {
+        expect(romanToDecimalConverter((input))).toBe(expected);
     })
 })
