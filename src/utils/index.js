6f16713720d6ad8b66c4843877d3e90a27ea6435
Brandon Donato-Long <wartornhero@gmail.com>
2021-04-19 22:13:31 +0200

    save off more broad converter for Roman to Decimal working up to 50 before refactor

--- src/utils/index.js
+++ src/utils/index.js
@@ -19,3 +19,32 @@ export const decimal2RomanSingleDigitConverter = (digit) => {
 
     return romanNumeral
 }
+
+export const romanToDecimalSingleDigitConverter = (romanNumeral) => {
+    let decimal = 0;
+    let counter = romanNumeral
+
+    while(counter.length> 0) {
+        if( counter.indexOf('XL') === 0) {
+            counter = counter.replace('XL', "");
+            decimal += 40;
+        } else if( counter.indexOf('X') === 0) {
+            counter = counter.replace('X', "");
+            decimal += 10;
+        } else if (counter.indexOf('IX') === 0) {
+            counter = counter.replace('IX', "")
+            decimal += 9
+        } else if (counter.indexOf('V') === 0 ) {
+            counter = counter.replace('V', "")
+            decimal += 5;
+        }else if ( counter.indexOf('IV') === 0 ) {
+            counter = counter.replace('IV', "")
+            decimal += 4;
+        } else if (counter.indexOf('I') === 0 ){
+            counter = counter.replace('I', "")
+            decimal++;
+        }
+    }
+
+    return decimal
+}
--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,7 +1,13 @@
-import {decimal2RomanSingleDigitConverter} from "./index";
+import {decimal2RomanSingleDigitConverter, romanToDecimalSingleDigitConverter} from "./index";
 
-describe('singleDigitConverter', () =>{
+describe.skip('singleDigitConverter', () =>{
     it.each([[-1, ''],[0, ''],[1,'I'],[2,'II'],[4, 'IV'],[5, 'V'],[7, 'VII'],[8, 'VIII'],[9, 'IX']])('test RomanNumeralization for singles digit. %i', (input, expected) => {
         expect(decimal2RomanSingleDigitConverter(input)).toBe(expected);
+    })
+})
+
+describe('RomanNumeral to Decimal Single Digit Converter', () => {
+    it.each([['XL', 40],['XXI', 21],['XIX', 19],['XI', 11],['X', 10], ['IX', 9], ['VIII', 8], ['V', 5], ['IV', 4], ['III', 3]])('test decimalization of roman numerals: %s', (input, expected) => {
+        expect(romanToDecimalSingleDigitConverter((input))).toBe(expected);
     })
 })
